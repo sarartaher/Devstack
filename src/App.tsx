28084a261c3fd1783c5f72a5e56a8071c16d59de
sarar2003 <saifurtaher40@gmail.com>
2026-09-11 20:25:37 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <ToastContainer />
+    </>
+  )
+}
+
+export default App
